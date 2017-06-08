e40727000437fefd884fc4e0e7d7d71123d20895
Didier Dubois <code@didier-dubois.ch>
2017-06-08 15:43:59 +0200

    Create commands.sh

--- src/ch/dd/interview/commands.sh
+++ src/ch/dd/interview/commands.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
+# Syntaxe de find > grep dans un fichier?
+find .  .....
+
+# Que fait cette commande?
 [ $? -eq 1 ] && ps
+
+# comment compter toutes les lignes "ERROR" dans tous les fichiers *.log
+
